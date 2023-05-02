b16372ee1a69335eecfc5a4042efd1202f298b7b
Praise <praiseoluyale20@gmail.com>
2023-05-02 17:21:41 +0100

    update

--- src/Project.js
+++ src/Project.js
@@ -16,6 +16,8 @@ function Header(){
         <li>Business Card: project to learn react basics <button><a  className='link'  href='https://profile-one-red.vercel.app/' target='_blank'>View</a></button></li>
         <li>Short stays website: projects focused on props and map() <button><a className='link' href='https://rentals-sooty.vercel.app' target='_blank'>View</a></button></li>
         <li>Travel Bucketlist website: projects that's also focused on props and map() <button><a className='link' href='https://travel-sand-iota.vercel.app/' target='_blank'>View</a></button></li>
+        <li>Counter webpage: projects focused on useState <button><a className='link' href='https://counter-react-taupe.vercel.app' target='_blank'>View</a></button></li>
+        <li>Meme generator: projects focused useState,conditional rendering, forms and useEffect <button><a className='link' href='https://meme-generator-rho-lake.vercel.app/' target='_blank'>View</a></button></li>
       </ul>
       </>
     )
